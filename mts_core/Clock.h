f51c57891ff255f26294cc5426fbccffb3e97d96
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-17 18:45:57 -0400

    clock header defineq

--- mts_core/Clock.h
+++ mts_core/Clock.h
@@ -5,3 +5,29 @@
 #include "TimerCallback.h"
 #include "DateCallback.h"
 
+namespace mts{
+    class MTS_CORE_API Clock:public Component{
+        public:
+            Clock(void);
+            virtual ~Clock(void);
+            virtual DateTime now() const = 0;
+            virtual int currentTradingDay() const = 0;
+
+            int createTimer(int beginTime, int interval, TimerCallback*);
+            bool destroyTimer(int id);
+
+            void addDateCallback(DateCallback*);
+        protected:
+            virtual QObject* doCreateTimer(int id, qint64 beginTicks, int interval, TimerCallback*) = 0;
+            virtual bool doDestroyTimer(QObject*) = 0;
+            void clear();
+            void notifyBusinessDateChanged(int newBusinessDate);
+        private:
+            QList<QObject*> _ids;
+            QList<DateCallback*> _dateCallbacks;
+#ifdef MTS_UNIT_TEST
+        public:
+#endif
+            qint64 calcBeginUTCTicks(int beginTime, int interval) const;
+    };
+}
