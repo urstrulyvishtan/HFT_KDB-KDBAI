4969c3151b6adab65dad1fc05fa8eeafc7ac2f5b
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-17 18:25:06 -0400

    component header

--- mts_core/Component.h
+++ mts_core/Component.h
@@ -1,3 +1,24 @@
 #pragma once
 #include <QtCore/QObject>
 #include "mts_core_api.h"
+
+class MTS_CORE_API Component:public QObject{
+    Q_OBJECT
+public:
+    Component();
+    virtual ~Component();
+
+    virtual bool initialize(const QVariantMap& params)=0;
+    bool isInitialized() const;
+
+public Q_SLOTS:
+    void emitInitalizeDone();
+    void emitInitializeError(const QString& err);
+
+Q_SIGNALS:
+    void initalizeDone();
+    void initalizeError(const QString& err);
+
+private:
+    bool m_initialized;
+}
