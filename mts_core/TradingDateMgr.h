5c28b6f8d3c53f68b6516e338279b682665bfe0d
Sibi Vishtan <thirukonda.s@northeastern.edu>
2024-08-24 18:44:36 -0400

    create Trading date manager header

--- mts_core/TradingDateMgr.h
+++ mts_core/TradingDateMgr.h
@@ -5,3 +5,35 @@
 #include <QtCore/QPair>
 #include "base/DateTime.h"
 #include "mts_core/enums.h"
+
+namespace mts{
+    enum TradingDateTimeCheckMode{
+        TCM_EXACT,
+        TCM_ROUND
+    };
+    class MTS_CORE_API TradingDateMgr{
+    public:
+        static bool init(const QString& configDir, bool is24H);
+        static TradingDateMgr* instance();
+
+        virtual int nextDate(int dt) const = 0;
+        virtual int prevDate(int dt) const = 0;
+        virtual int forceToDate(int dt) const = 0;
+
+        virtual int tradingDate(const DateTime& realDateTime) const = 0;
+        virtual bool isTradingDate(int dt) const = 0;
+        virtual bool isHalfTradingDate(int dt) const = 0;
+        virtual bool isTradingTime(const DateTime& dt, TradingDateTimeCheckMode = TCM_EXACT) const = 0;
+
+        virtual int realDate(int tradingDate, int realTime) const = 0;
+
+        virtual int nextDate(int dt, int n) const = 0;
+        virtual int countTradingDay(int begDt, int endDt) = 0;
+
+        virtual int openTradingTime(int dt = INVALID_DATE) const = 0;
+        virtual int closeTradingTime(int dt = INVALID_DATE) const = 0;
+        virtual DateTime nextTime(int futureTime, const DateTime& baseTime) const = 0;
+    private:
+        static TradingDateMgr* _instance;
+    };
+}
